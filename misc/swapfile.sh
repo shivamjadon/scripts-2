6de3f25c8ba93b1021def5726b5fc25b7c2d7f67
Dimitar Yurukov <mscalindt@protonmail.com>
2020-08-02 04:09:58 +0300

    swapfile: Tag essential variables
    Signed-off-by: Dimitar Yurukov <mscalindt@protonmail.com>

--- misc/swapfile.sh
+++ misc/swapfile.sh
@@ -6,11 +6,11 @@
  *   Create swap file.
  *
  * Usage:
- *   BLOCK_SIZE:
+ *   BLOCK_SIZE: [essential]
  *   Set block size for the swap file. Can be set in bytes and human readable
  *   form (K - KiB, M - MiB, G - GiB, ...), 'man dd' for more info.
  *
- *   BLOCKS_COUNT:
+ *   BLOCKS_COUNT: [essential]
  *   Set count of blocks. Can also be set in human readable form
  *   (K - KiB, M - MiB, G - GiB, ...), 'man dd' for more info.
  *
